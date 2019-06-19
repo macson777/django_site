a1fcd6fc1f88845d37aab65e2e78ac2cd24907ae
Aniperko Maksim Alex <MAniperko@mks.local>
2019-06-19 16:53:55 +0300

    'telegram_from'

--- djangosite/siteapp/urls.py
+++ djangosite/siteapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from siteapp.views import base_view, category_view, product_view
+from siteapp.views import base_view, category_view, product_view, telegram_form
 
 urlpatterns = [
     path('', base_view, name='base'),
     path('category/<category_slug>/', category_view, name='category_detail'),
     path('product/<product_slug>/', product_view, name='product_detail'),
+    path('feedback/', telegram_form, name='telegram_form'),
 ]
 
 
--- djangosite/siteapp/views.py
+++ djangosite/siteapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from siteapp.models import Category, Product
+import telegram
 
 # Create your views here.
 
@@ -28,3 +29,30 @@ def category_view(request, category_slug):
         'category': category
     }
     return render(request, 'category.html', context)
+
+
+def telegram_form(request):
+    token = "759065563:AAHyNPXxXKLI7jn52uc55QWU2aQHLBWMXVE"
+    chat_id = '-393705029'
+    bot = telegram.Bot(token=token)
+    if request.method == 'GET':
+        return render(request, 'telegram_feedback_form.html')
+    name = request.POST.get('user_name')
+    phone_number = request.POST.get('user_phone')
+    email = request.POST.get('user_email')
+    message = request.POST.get('user_message')
+
+    context = {
+        'Имя пользователя:': name,
+        'Телефон:': phone_number,
+        'Email:': email,
+        'Сообщение:': message,
+    }
+
+    text = ''
+    for key, value in context.items():
+        text += f'{key} {value}\n'
+
+    bot.send_message(chat_id=chat_id, text=text)
+    return redirect('http://127.0.0.1:8000')
+
